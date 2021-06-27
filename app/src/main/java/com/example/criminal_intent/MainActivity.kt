a38ff3f620193e26c7f34aae68a31302f0d8d162
Maxim Tolstobrov <tmaksim1989@gmail.com>
2021-06-27 18:39:53 +0300

    Create crime list fragment

--- app/src/main/java/com/example/criminal_intent/MainActivity.kt
+++ app/src/main/java/com/example/criminal_intent/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.criminal_intent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.criminal_intent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val s : Int = 0
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment == null) {
+            val fragment = CrimeFragment()
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment)
+                .commit()
+        }
+
     }
 }
